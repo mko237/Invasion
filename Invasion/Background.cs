4354fbe54ee92b1d31c23a6186a8bd3e80e22f51
mko237 <mko237@yahoo.com>
2014-03-20 17:36:36 -0400

    started background

---
+++ Invasion/Background.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Invasion
+{
+    class Background : Entity
+    {
+        private static Rectangle imageBox = new Rectangle();
+        public Background()
+        {
+            image = Art.Planet;
+            Position = GameRoot.ScreenSize / 2;
+            Orientation = 360f;
+        }
+
+        public void Update()
+        {
+                   
+                if(Orientation <= 0)
+                    Orientation -= .05f;
+                else
+                    Orientation = 360;
+               
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            
+        }
+    }
+
+}
