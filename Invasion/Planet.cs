0aadeee2e1c1785496cb3517f6878ad08ef082f4
Crysco <crysrice.21@gmail.com>
2014-03-24 15:17:47 -0700

    Planet Overlay Color Health stuff 2.0

--- Invasion/Planet.cs
+++ Invasion/Planet.cs
@@ -23,8 +23,13 @@ namespace Invasion
         private const float productionAccel = 2.5f;
         private float productionRate;
         public float shipCount { get; set; }
-        public Team team;
+        public float enemyShipCount { get; set; }
+        public Team team { get; private set; }
+        public Team invadingTeam { get; set; }
         public State state { get; set; }
+
+
+        private bool beingInvaded = false;
 
         public Planet()
         {
@@ -39,12 +44,14 @@ namespace Invasion
             color = col;
             ObjectSize = size;
             Radius = (0.5f * LevelSpawner.imageSize * size);
-            image = Art.Planet;
-            productionRate = productionAccel * 2 * Radius - 50; 
+            productionRate = productionAccel * 2 * Radius - 50;
             ID = id;
-            Vector2 textSize = text.MeasureString(ID.ToString());
-            centerTextOffset = (textSize) * size / 2;
             this.team = team;
+            this.invadingTeam = team;
+
+            image = Art.Planet;
+            Vector2 textSize = text.MeasureString(ID.ToString());
+            centerTextOffset = (textSize) * ObjectSize / 2;
 
             if (team == null)
             {
@@ -58,23 +65,47 @@ namespace Invasion
             }
         }
 
-        public void changeTeams(Team team)
+        public void changeTeams()
         {
-            this.team = team;
+            this.team = invadingTeam;
             color = team.getColor();
         }
 
         public override void Update()
         {
+            if (enemyShipCount > 0)
+                beingInvaded = true;
+            else
+            {
+                enemyShipCount = 0;
+                beingInvaded = false;
+            }
+
             if (team != null)
+            {
                 shipCount += (productionRate / 3600);
+                enemyShipCount -= (productionRate / 3600);
+            }
+
+            if (shipCount < 0)
+                changeTeams();
+
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(image, Position, null, color, Orientation, Size / 2f, ObjectSize, 0, 0);  
+            if (beingInvaded)
+            {
+                Rectangle top = new Rectangle(0, 0, LevelSpawner.imageSize, (int)(LevelSpawner.imageSize * (enemyShipCount / (enemyShipCount + shipCount))));
+                Rectangle bottom = new Rectangle(0, (int)(LevelSpawner.imageSize * (enemyShipCount / (enemyShipCount + shipCount))), LevelSpawner.imageSize, (int)(LevelSpawner.imageSize*(shipCount / (enemyShipCount + shipCount))));
+                spriteBatch.Draw(image, Position, top, invadingTeam.getColor(), Orientation, Size / 2f, ObjectSize, 0, 0);
+                spriteBatch.Draw(image, new Vector2(Position.X, Position.Y + ObjectSize * (int)(LevelSpawner.imageSize * (enemyShipCount / (enemyShipCount + shipCount)))), bottom, color, Orientation, Size / 2f, ObjectSize, 0, 0);
+            }
+            else
+                spriteBatch.Draw(image, Position, null, color, Orientation, Size / 2f, ObjectSize, 0, 0);
+
             spriteBatch.DrawString(text, ID.ToString(), Position-centerTextOffset, Color.White,Orientation,new Vector2(0,0),ObjectSize,0,0);
-            spriteBatch.DrawString(text, shipCount.ToString(), Position, Color.White, Orientation, new Vector2(0, 0), ObjectSize, 0, 0);
+            spriteBatch.DrawString(text, shipCount.ToString("0"), Position, Color.White, Orientation, new Vector2(0, 0), ObjectSize, 0, 0);
         }
 
             
